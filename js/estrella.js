60af6950269485b865267a700fe098903a562af0
Konra <82560729+iKonra@users.noreply.github.com>
2023-03-19 22:56:17 -0300

    Add files via upload

---
+++ js/estrella.js
@@ -0,0 +1,75 @@
+const background = document.querySelector('.background-container');
+const generateStarsBtn = document.querySelector('#generate-stars-btn');
+const starCounter = document.querySelector('#star-counter');
+let numStars = 0;
+let starSize = 1;
+let isGeneratingPlanets = false;
+
+
+
+function generateStar() {
+  let element;
+  if (Math.random() < 0.05) { 
+    element = generateAsteroid();
+  } else {
+    element = generateStarElement();
+  }
+  background.appendChild(element);
+  
+  numStars++;
+  starCounter.innerText = numStars;
+
+  if (numStars % 100 === 0 && numStars <= 1000) {
+    starSize += 0.05;
+    if (!isGeneratingPlanets) {
+      isGeneratingPlanets = true;
+      if (numStars === 1000) {
+        generateStarsBtn.innerText = 'Generate Planets';
+      }
+    }
+    generatePlanet();
+  }
+}
+
+function generateStarElement() {
+  const star = document.createElement('div');
+  star.classList.add('star');
+  star.style.top = `${Math.random() * 100}%`;
+  star.style.left = `${Math.random() * 100}%`;
+  star.style.transform = `scale(${starSize})`;
+  return star;
+}
+
+function generateAsteroid() {
+  const asteroid = document.createElement('div');
+  asteroid.classList.add('asteroid');
+  asteroid.style.top = `${Math.random() * 100}%`;
+  asteroid.style.left = `${Math.random() * 100}%`;
+
+  // movimiento de asteroides
+  const intervalId = setInterval(() => {
+    const currentTop = parseInt(asteroid.style.top);
+    const currentLeft = parseInt(asteroid.style.left);
+    const newTop = currentTop + 2; // velocidad de movimiento en píxeles
+    const newLeft = currentLeft + 2; // velocidad de movimiento en píxeles
+    asteroid.style.top = `${newTop}%`;
+    asteroid.style.left = `${newLeft}%`;
+
+    if (newTop > 100 || newLeft > 100) { // cuando el asteroide sale de la pantalla, se detiene el intervalo
+      clearInterval(intervalId);
+      asteroid.remove();
+    }
+  }, 30); // intervalo de tiempo en milisegundos
+  
+  return asteroid;
+}
+
+function generatePlanet() {
+  const planet = document.createElement('div');
+  planet.classList.add('planet');
+  planet.style.top = `${Math.random() * 100}%`;
+  planet.style.left = `${Math.random() * 100}%`;
+  background.appendChild(planet);
+}
+
+generateStarsBtn.addEventListener('click', generateStar);
---
+++ js/nombre.js
@@ -0,0 +1,13 @@
+
+
+const nick = document.querySelector('#nick');
+
+nick.addEventListener('click', () => {
+  anime({
+    targets: nick,
+    translateY: 50,
+    opacity: 0,
+    duration: 1000,
+    easing: 'easeOutExpo'
+  });
+});
